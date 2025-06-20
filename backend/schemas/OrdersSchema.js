7a4c6cad2bd386834131c499867cdd9703872f2a
Vikrant Shinde <vikrantshinde8855@gmail.com>
2025-06-20 11:48:47 +0530

    Initial commit - Zerodha Clone

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,11 @@
+// ../schemas/OrdersSchema.js
+const mongoose = require("mongoose");
+
+const OrdersSchema = new mongoose.Schema({
+  name: String,
+  qty: Number,
+  price: Number,
+  mode: String,
+});
+
+module.exports = OrdersSchema;
